d3376058f0bc4a411d7d40dfbadf9cb51dfb58c3
Sichacvah <sichirc@gmail.com>
2015-03-23 00:47:51 +0500

    revision hound check

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -1,7 +1,6 @@
 SimpleForm.setup do |config|
-
-  config.wrappers :default, class: :input,
-    hint_class: :field_with_hint, error_class: :field_with_errors do |b|
+  config.wrappers :default, class: :input, hint_class: :field_with_hint,
+                            error_class: :field_with_errors do |b|
 
     b.use :html5
 
