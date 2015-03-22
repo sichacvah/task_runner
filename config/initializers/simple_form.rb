aa95aef5bdf335446212e931d76d20289b788433
Sichacvah <sichirc@gmail.com>
2015-03-23 00:49:06 +0500

    revision hound check

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -1,7 +1,6 @@
 SimpleForm.setup do |config|
   config.wrappers :default, class: :input, hint_class: :field_with_hint,
                             error_class: :field_with_errors do |b|
-
     b.use :html5
 
     b.use :placeholder
@@ -15,22 +14,21 @@ SimpleForm.setup do |config|
     b.optional :readonly
 
     b.use :label_input
-    b.use :hint,  wrap_with: { tag: :span, class: :hint }
+    b.use :hint, wrap_with: { tag: :span, class: :hint }
     b.use :error, wrap_with: { tag: :span, class: :error }
-
   end
 
   config.default_wrapper = :default
 
   config.boolean_style = :nested
 
-  config.button_class = 'btn'
+  config.button_class = "btn"
 
   config.error_notification_tag = :div
 
-  config.error_notification_class = 'error_notification'
+  config.error_notification_class = "error_notification"
 
   config.browser_validations = false
 
-  config.boolean_label_class = 'checkbox'
+  config.boolean_label_class = "checkbox"
 end
